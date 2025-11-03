c56b24c4a3efcb86996e6bfe7bb562547a607242
jakub <j.bochenski@zoovu.com>
2025-11-03 18:19:48 +0100

    test: Refuse negative bets

--- contract/jackpot-bets/consumers/example/place_bet.groovy
+++ contract/jackpot-bets/consumers/example/place_bet.groovy
@@ -1,29 +1,50 @@
 import static org.springframework.cloud.contract.spec.Contract.make
 
-make {
-    request {
-        method POST()
-        url "/bets"
-        body(
-            betId: 1,
-            userId: 1,
-            jackpotId: 1,
-            amount: 50,
-        )
-        headers {
-            contentType(applicationJson())
+[
+    make {
+        request {
+            method POST()
+            url "/bets"
+            body(
+                betId: 1,
+                userId: 1,
+                jackpotId: 1,
+                amount: 50,
+            )
+            headers {
+                contentType(applicationJson())
+            }
         }
-    }
-    response {
-        status ACCEPTED()
-        body(
-            betId: 1,
-            userId: 1,
-            jackpotId: 1,
-            amount: 50,
-        )
-        headers {
-            contentType(applicationJson())
+        response {
+            status ACCEPTED()
+            body(
+                betId: 1,
+                userId: 1,
+                jackpotId: 1,
+                amount: 50,
+            )
+            headers {
+                contentType(applicationJson())
+            }
         }
-    }
-}
+    },
+    make {
+        description "Cannot place a bet with negative amount"
+        request {
+            method POST()
+            url "/bets"
+            body(
+                betId: 1,
+                userId: 1,
+                jackpotId: 1,
+                amount: -50,
+            )
+            headers {
+                contentType(applicationJson())
+            }
+        }
+        response {
+            status BAD_REQUEST()
+        }
+    },
+]
